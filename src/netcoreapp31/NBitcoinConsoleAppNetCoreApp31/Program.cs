5d66257f3e5dc0195ced1feeca5e12cd46655730
C. Augusto Proiete <augusto@proiete.com>
2021-09-26 21:01:33 -0300

    Initial version

---
+++ src/netcoreapp31/NBitcoinConsoleAppNetCoreApp31/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NBitcoin;
+
+namespace NBitcoinConsoleAppNetCoreApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Environment.SetEnvironmentVariable("HOME", null, EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("APPDATA", null, EnvironmentVariableTarget.Process);
+
+            Console.WriteLine("---");
+            Console.WriteLine("Generating NBitcoin Key");
+
+            try
+            {
+                Console.WriteLine($"  Generated key => {GenerateKey()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+                Console.WriteLine();
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine("---");
+        }
+
+        private static string GenerateKey()
+        {
+            var privateKey = new Key(); // generate a random private key
+            var publicKey = privateKey.PubKey;
+
+            return publicKey.GetAddress(ScriptPubKeyType.Legacy, Network.Main).ToString();
+        }
+    }
+}
